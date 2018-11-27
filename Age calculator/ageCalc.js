c5db32c84ea911066701e5d1bd78bf4b117775ce
Mike S <rxy369@gmail.com>
2018-11-27 09:17:50 -0800

    Added source fiiles

---
+++ Age calculator/ageCalc.js
@@ -0,0 +1,3 @@
+var age = prompt("How old are you?");
+alert("Wow! " + age + " years!  You've been alive for " + (age * 356.25) + " days! :O");
+console.log("Wow!" + age + " years! You've been alive for about " + (age * 356) + " days! :O");
